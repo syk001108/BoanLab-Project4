5ee244eab3bba11e8d8f467296529d7d90dc5e70
HOXYKIM <57708995+syk001108@users.noreply.github.com>
2023-02-13 02:19:41 +0900

    Update html tag remove & modify

--- Crawling/sk_Algospot_crawler.py
+++ Crawling/sk_Algospot_crawler.py
@@ -3,12 +3,41 @@ import urllib.request
 import time
 import random
 import csv
+import re
 
 def get_soup(target_url):
     html = urllib.request.urlopen(target_url).read()
     time.sleep(random.uniform(2,5))
     soup = BeautifulSoup(html, 'html.parser')
     return soup
+
+def remove_html(sentence):
+    sentence.find('h4').decompose()
+    p = sentence.find_all('span')
+    sentence = str(sentence)
+    for i in p:
+        sentence = sentence.replace(str(i), '')
+    sentence = sentence.replace('<sup>', '^{')
+    sentence = sentence.replace('</sup>', '}')
+    sentence = sentence.replace('<sub>', '_{')
+    sentence = sentence.replace('</sub>', '}')
+    sentence = re.sub('(<([^>]+)>)', '', sentence)
+    sentence = sentence.replace('&lt;', '<')
+    sentence = sentence.replace('\lt', '<')
+    sentence = sentence.replace('&gt;', '>')
+    sentence = sentence.replace('\gt', '>')
+    sentence = sentence.replace('&le;', '≤')
+    sentence = sentence.replace('\le', '≤')
+    sentence = sentence.replace('&ge;', '≥')
+    sentence = sentence.replace('\ge', '≥')
+    sentence = sentence.replace('&Hat;', '^')
+    sentence = sentence.replace('&apos;', '`')
+    sentence = sentence.replace('&semi;', ';')
+    sentence = sentence.replace('&amp;', '&')
+    sentence = sentence.replace('&quot;', '"')
+    sentence = sentence.replace('&num;', '#')
+    sentence = sentence.strip()
+    return sentence
 
 def info_data(soup):
     head = soup.find('div', {'class': 'article-container'})
@@ -21,34 +50,34 @@ def info_data(soup):
     memory = lis[2].text.strip()
     li1 = soup.find('a', {'class': 'username'})
     author = li1.text.strip()
-    li2 = lic.find('li', {'class': 'source'})
-    li2s = li2.find('a')
-    source = li2s.text.strip()
-    print(ID, name, time, memory, author, source)
+    return(ID, name, time, memory, author)
 
 def problem_data(soup):
-    section = soup.find('section', {'class': 'problem_sample_input'})
-    print(section)
+    section = soup.find('section', {'class': 'problem_statement'})
+    section = remove_html(section)
+    return section
 
 def input_data(soup):
-    section = soup.find('section', {'class': 'problem_statement'})
-    print(section)
+    section = soup.find('section', {'class': 'problem_input'})
+    section = remove_html(section)
+    return section
 
 def output_data(soup):
-    section = soup.find('section', {'class': 'problem_input'})
-    print(section)
+    section = soup.find('section', {'class': 'problem_output'})
+    section = remove_html(section)
+    return section
 
 def sample_input_data(soup):
-    section = soup.find('section', {'class': 'problem_output'})
+    section = soup.find('section', {'class': 'problem_sample_input'})
     pres = section.find_all('pre')
-    input = pres[0].text.strip()
-    print(input)
+    input_d = pres[0].text.strip()
+    return input_d
 
 def sample_output_data(soup):
     section = soup.find('section', {'class': 'problem_sample_output'})
     pres = section.find_all('pre')
-    input = pres[0].text.strip()
-    print(input)
+    output_d = pres[0].text.strip()
+    return output_d
 
 def extract_data(soup):
     table = soup.find('table', {'class': 'problem_list'})
@@ -59,12 +88,12 @@ def extract_data(soup):
             target_url = 'https://www.algospot.com/judge/problem/read/'+tds[1].text.strip()
             time.sleep(random.uniform(2,4))
             soup = get_soup(target_url)
-            info_data(soup)
-            problem_data(soup)
-            input_data(soup)
-            output_data(soup)
-            sample_input_data(soup)
-            sample_output_data(soup)
+            info = info_data(soup)
+            problem = problem_data(soup)
+            input_d = input_data(soup)
+            output_d = output_data(soup)
+            sam_in_d = sample_input_data(soup)
+            sam_out_d = sample_output_data(soup)
 
 target_url = 'https://www.algospot.com/judge/problem/list/1'
 soup = get_soup(target_url)
